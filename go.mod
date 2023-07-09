7bc849679b8046bfc2c132d25123fc26fb337d9c
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-07-09 21:59:56 +0200

    init

---
+++ go.mod
@@ -0,0 +1,7 @@
+module gossr
+
+go 1.20
+
+require github.com/go-chi/chi/v5 v5.0.8
+
+require github.com/lib/pq v1.10.9
---
+++ server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/health-check", healthCheck)
+	router.Get("/index", view)
+	router.Get("/output.css", styles)
+
+	log.Default().Println("Listening on port 7007.")
+	http.ListenAndServe(":7007", router)
+}
+
+func styles(w http.ResponseWriter, r *http.Request) {
+	type PageData struct {
+	}
+	tmpl := template.Must(template.ParseFiles("view/styles/output.css"))
+
+	tmpl.Execute(w, PageData{})
+}
+
+func view(w http.ResponseWriter, r *http.Request) {
+	type PageData struct {
+		Name string
+	}
+	tmpl := template.Must(template.ParseFiles("view/index.html"))
+
+	tmpl.Execute(w, PageData{
+		Name: "World!",
+	})
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
